728ff3c540d7ff90664d1891d890265655abbd50
Julia Chanas <juulia.chanas@gmail.com>
2023-10-15 14:48:15 +0200

    Add subpages

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,67 @@
 //importy na poczatku pliku!
-import { settings, select } from './settings.js'; //kropka i slash bardzo wazne - sciezka do pliku!
+import { settings, select, classNames } from './settings.js'; //kropka i slash bardzo wazne - sciezka do pliku!
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+    //wszystkie dzieci kontenera stron beda we wlasciwosci pages
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    //znajdz wsyztskie linki
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromhash = window.location.hash.replace('#/', '');
+
+    //thisApp.activatePage(thisApp.pages[0].id); //wyswietlu sie pierwsza z podstron
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id == idFromhash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash); // zeby przy odswiezaniu nie wracala do poprzejdniej podstrony
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        //w stalej id chcemy zapisac atrybut href kliknietego eleementu, w ktorym zamienimy # na pusty ciag znakow
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /*run this App.activatePage with that id*/
+        thisApp.activatePage(id);
+
+        /* change url hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for (let page of thisApp.pages) {
+      // nadaje klase podana jako pierwszy argument jesli jej nie było i odbiera kied ejst
+      //drugi argument kontroluje czy klasa ma zostac nadana czy tez nie!!
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* add class "active" to matching links, remove from non-matching */
+    for (let link of thisApp.navLinks) {
+      //dla kazdego z linkow zapisanych w thisApp.navLinks dodaj lub usun klase w className.nav.active w zaleznosci od tego czy atrybut href tego linka jest rowny "#" oraz id podstrony podanej jako argument axctive page
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
   initMenu: function () {
     const thisApp = this;
     console.log('thisApp.data ', thisApp.data);
@@ -57,11 +115,12 @@ const app = {
   init: function () {
     const thisApp = this;
     console.log('*** App starting ***');
-    console.log('thisApp:', thisApp);
+    //console.log('thisApp:', thisApp);
     //console.log('classNames:', classNames);
-    console.log('settings:', settings);
+    //console.log('settings:', settings);
     //console.log('templates:', templates);
 
+    thisApp.initPages();
     thisApp.initData();
     //thisApp.initMenu();
     thisApp.initCart();
